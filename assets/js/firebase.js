77f8929ed0e543c67437db2bb203509dc8112fbe
SaraaS2 <saraalavinee@gmail.com>
2024-07-09 16:49:08 -0300

    Criação da função de envio de dados para o banco de dados

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
 import {getAuth} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+import {getDatabase} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
 //import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
   apiKey: "AIzaSyA-puQK16VJnvn_qQAozzvyh2pKhkr_aiY",
@@ -9,11 +10,13 @@ const firebaseConfig = {
   storageBucket: "idas-54ac4.appspot.com",
   messagingSenderId: "942140932496",
   appId: "1:942140932496:web:59628e9757e7df1f7fb315",
-  measurementId: "G-7VLME4005R"
+  measurementId: "G-7VLME4005R",
+  databaseURL: "https://idas-54ac4-default-rtdb.firebaseio.com"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
+const database = getDatabase(app)
 
-export {app, auth}
+export {app, auth, database}
